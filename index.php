0cdc9ced7f1d0196602539a0d045493c300baa98
Danilo Cabello <danilo.cabello@gmail.com>
2012-05-18 20:06:09 -0300

    Try a query

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
 
-echo "Hi {$_GET['name']}";
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+mysql_connect(
+        $server = $url["host"],
+        $username = $url["user"],
+        $password = $url["pass"]);
+        $db=substr($url["path"],1);
+
+mysql_select_db($db);
+
+mysql_query('CREATE  TABLE IF NOT EXISTS `service` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(255) NULL ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) )
+ENGINE = InnoDB;');
+
+mysql_query("INSERT INTO service (name) VALUES ('Diarista');");
+
+$result = mysql_query("SELECT * FROM service;");
+if ($result) {
+    while ($row = mysql_fetch_assoc($result)) {
+        echo $row['id'];
+        echo $row['name'];
+    }
+}
 
 ?>
