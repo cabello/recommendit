4af032d00c08c4589d9e2b199c415b303fb29732
Thiago Genez <thiagogenez@gmail.com>
2012-05-19 05:02:41 -0300

    aling

--- kickstrap/home.php
+++ kickstrap/home.php
@@ -111,7 +111,7 @@ oauth      : true
 
         <div class="fb-login-button" scope="<?php echo $permittion_names ?>" style="text-align: center; display: none;">Login with Facebook</div>
         <div style="margin: auto; width:200px">
-          <div class="fb-like" data-href="http://fb-hacktoon.herokuapp.com/" data-send="true" data-width="450" data-show-faces="true" data-font="segoe ui"></div>
+          <div class="fb-like" data-href="http://fb-hacktoon.herokuapp.com/" data-send="true" data-width="450" data-show-faces="false" data-font="segoe ui"></div>
         </div>
         <hr>
 
