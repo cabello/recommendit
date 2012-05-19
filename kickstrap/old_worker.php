b992b842fcfe597a1b131e40602caa08c974531f
Danilo Cabello <danilo.cabello@gmail.com>
2012-05-19 07:18:15 -0300

    blabla

---
+++ kickstrap/old_worker.php
@@ -0,0 +1,55 @@
+
+
+<?php
+	session_start();
+	$_SESSION['user_id']="33";
+	$service = 1;
+
+// Nome, telefone, rating, comment
+?>
+
+
+
+
+<?php
+
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+mysql_connect(
+        $server = $url["host"],
+        $username = $url["user"],
+        $password = $url["pass"]);
+        $db=substr($url["path"],1);
+
+mysql_select_db($db);
+
+ini_set("display_errors", "1");
+error_reporting(E_ALL);
+
+
+	if(isset($_POST['rating'])){
+		$SQLQuery = "INSERT INTO recommendation (id_worker, id_facebook, id_service, rating, comment) VALUES ('".$_GET['id_worker']."', '".$_SESSION['user_id']."', '".$_POST['service']."', '".$_POST['rating']."', '".$_POST['comment']."')";
+		echo $SQLQuery;
+		mysql_query($SQLQuery);
+	}
+
+
+?>
+
+
+<?php
+if (count($_POST) > 0) {
+  print json_encode($_POST);
+  die();
+}
+?>
+
+
+<div>
+	<form name="add_new" action="#" method="post">
+		<!-- <input type="hidden" name="id_worker" value="1" /><br> -->
+		<input type="hidden" name="service" value="1" />
+		Rating: <input type="text" name="rating" required="required" /><br>
+		Comentario: <input type="text" name="comment" required="required" /><br>
+		<input type="submit" value="Submit" />
+	</form>
+</div>
