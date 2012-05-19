9b7166f4381329159bcbd45a65b10b9a417336c0
Danilo Cabello <danilo.cabello@gmail.com>
2012-05-19 03:32:54 -0300

    Testing

---
+++ kickstrap/home.php
@@ -0,0 +1,158 @@
+<?php session_start(); ?>
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+
+  <!-- Use the .htaccess and remove these lines to avoid edge case issues.
+       More info: h5bp.com/i/378 -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title></title>
+  <meta name="description" content="">
+  <!-- Mobile viewport optimized: h5bp.com/viewport -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
+  <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <!-- More ideas for your head here: h5bp.com/d/head-Tips -->
+
+  <!-- For the sticky footer -->
+	<!--[if !IE 7]><style type="text/css">#sf-wrapper {display:table;height:100%}</style><![endif]-->
+
+  <!-- All JavaScript at the bottom, except this Modernizr build.
+       Modernizr enables HTML5 elements & feature detects for optimal performance.
+       Create your own custom Modernizr build: www.modernizr.com/download/ -->
+  <script src="extras/h5bp/js/libs/modernizr-2.5.3.min.js"></script>
+</head>
+<body>
+  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
+       chromium.org/developers/how-tos/chrome-frame-getting-started -->
+  <!--[if lt IE 7]>
+    <link href="css/bootstrap.ie6.css" rel="stylesheet">
+  	<p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p>
+  <![endif]-->
+<div id="sf-wrapper">
+	<div class="container main">
+  	<header>
+
+  	</header>
+
+    <div role="main">
+        <div class="row-fluid">
+            <div class="span2">&nbsp;</div>
+            <div class="span8">
+              <div class="hero-unit">
+                <h1><i class="icon-list"></i> hacktoon</h1>
+                <h2>De amigo pra amigo.</h2>
+
+<div id="fb-root"></div>
+<script>
+<?php
+  $permittion_names = "read_friendlists";
+  $last_param = "_unique_are_you";
+
+  $app_secret = "51bb5cf55b06641f625a2e4f20e60334";
+  $app_id = "362571837137094";
+  $index_url = "http://fb-hacktoon.herokuapp.com/test/index.php";
+
+  $_SESSION["app_secret"] = $app_secret;
+  $_SESSION["app_id"] = $app_id;
+  $_SESSION["redirect_uri"] = $index_url;
+?>
+window.fbAsyncInit = function() {
+  FB.init({
+appId      : <?$app_id?>, // App ID
+status     : true, // check login status
+cookie     : true, // enable cookies to allow the server to access the session
+xfbml      : true  // parse XFBML
+});
+};
+
+// Load the SDK Asynchronously
+(function(d){
+ var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+ js = d.createElement('script'); js.id = id; js.async = true;
+ js.src = "//connect.facebook.net/en_US/all.js";
+ d.getElementsByTagName('head')[0].appendChild(js);
+ }(document));
+</script>
+
+<fb:login-button show-faces="true" width="400" height="1000" max-rows="1" scope="publish_actions">
+</fb:login-button>
+
+<div class="fb-login-button" data-show-faces="true" data-width="200" data-max-rows="1"></div>
+<?php
+//header("location: https://www.facebook.com/dialog/oauth?client_id=$app_id&redirect_uri=$index_url&scope=$permittion_names&state=$last_param");
+?>
+</p>
+
+                <p style="text-align: center"><a class="btn btn-primary btn-large">Login with Facebook &raquo;</a></p>
+              </div>
+            </div><!--/span-->
+            <div class="span2">&nbsp;</div>
+        </div><!--/row-fluid-->
+
+        <hr>
+
+        <div class="container">
+            <footer style="text-align: center">
+                <p><i class="icon-github-sign"></i> crafted in 24 hours during facebook@brazil hackathon 2012</p>
+            </footer>
+        </div>
+    </div>
+
+	  <!-- JavaScript at the bottom for fast page loading -->
+
+	  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+	  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	  <script>window.jQuery || document.write('<script src="extras/h5bp/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+	  <!-- scripts concatenated and minified via ant build script-->
+	  <script src="extras/prettify/prettify.js"></script>
+	  <script src="js/bootstrap.min.js"></script>
+
+	  <!-- end scripts-->
+
+	  <!-- If you'd rather pick and choose, comment the above and uncomment what you need below -->
+	  <!--script type="text/javascript" src="js/bootstrap-alert.min.js"></script-->
+	  <!--script type="text/javascript" src="js/bootstrap-button.min.js"></script-->
+	  <!--script type="text/javascript" src="js/bootstrap-carousel.min.js"></script-->
+	  <!--script type="text/javascript" src="js/bootstrap-collapse.min.js"></script-->
+	  <!--script type="text/javascript" src="js/bootstrap-dropdown.min.js"></script-->
+	  <!--script type="text/javascript" src="js/bootstrap-modal.min.js"></script-->
+	  <!--script type="text/javascript" src="js/bootstrap-modal.min.js"></script-->
+	  <!--script type="text/javascript" src="js/bootstrap-modal.min.js"></script-->
+	  <!--script type="text/javascript" src="js/bootstrap-popover.min.js"></script-->
+	  <!--script type="text/javascript" src="js/bootstrap-scrollspy.min.js"></script-->
+	  <!--script type="text/javascript" src="js/bootstrap-tab.min.js"></script-->
+	  <!--script type="text/javascript" src="js/bootstrap-tooltip.min.js"></script-->
+	  <!--script type="text/javascript" src="js/bootstrap-transition.min.js"></script-->
+	  <!--script type="text/javascript" src="js/bootstrap-typeahead.min.js"></script-->
+	  <!--script type="text/javascript" src=""></script-->
+
+	  <!-- end scripts -->
+
+	  <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
+	       mathiasbynens.be/notes/async-analytics-snippet -->
+
+	  <script type="text/javascript">
+	  // Use the modernizr.load() function to run polyfills for older browsers.
+	    Modernizr.load({
+
+	    });
+	  </script>
+	  <script>
+	    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+	    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	    s.parentNode.insertBefore(g,s)}(document,'script'));
+	  </script>
+  </div><!-- End .container.main -->
+</div><!-- End #sf-wrapper -->
+  <!-- A little ie7- magic -->
+  <script type="text/javascript"> $(function(){if($.browser.msie&&parseInt($.browser.version,10)===6){$('.row div[class^="span"]:last-child').addClass("last-child");$('[class="span"]').addClass("margin-left-20");$(':button[class="btn"], :reset[class="btn"], :submit[class="btn"], input[type="button"]').addClass("button-reset");$(":checkbox").addClass("input-checkbox");$('[class^="icon-"], [class=" icon-"]').addClass("icon-sprite");$(".pagination li:first-child a").addClass("pagination-first-child")}}) </script>
+</body>
+</html>
