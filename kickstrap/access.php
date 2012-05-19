11507f6e98c0d75bc3f60ed7cd1fe809ac8e8f1a
Danilo Cabello <danilo.cabello@gmail.com>
2012-05-19 04:46:58 -0300

    new worker form try 1

--- kickstrap/access.php
+++ kickstrap/access.php
@@ -3,4 +3,9 @@
 session_start();
 $_SESSION['token'] = $_POST['accessToken'];
 
+$query = "https://graph.facebook.com/me?access_token=".$_SESSION["token"];
+$response = file_get_contents($query);
+$user_data = json_decode($response, true);
+$_SESSION["user_id"] = $user_data["id"];
+
 ?>
