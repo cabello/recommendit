f6bc79e98d62df6c9ee5715e31f5ac69126c4081
Marcos <marcosgrappeggia@gmail.com>
2012-05-19 01:16:52 -0300

    asfdsfdfd

--- test/form_new.php
+++ test/form_new.php
@@ -16,9 +16,9 @@ error_reporting(E_ALL);
 
 
 	if(isset($_POST['name'])){
-		$SQLQuery = "INSERT INTO worker (name, phone, id_service) VALUES ('".$_POST['name']."', '".$_POST['telephone']."', 2)";
+		$SQLQuery = "INSERT INTO worker (name, phone, id_service) VALUES ('".$_POST['name']."', '".$_POST['telephone']."', '2')";
 		echo $SQLQuery;
-		mysql_query("INSERT INTO worker (name, phone, id_service) VALUES ('".$_POST['name']."', '".$_POST['telephone']."', 2)");
+		mysql_query("INSERT INTO worker (name, phone, id_service) VALUES ('".$_POST['name']."', '".$_POST['telephone']."', '2')");
 		
 	}
 
@@ -32,7 +32,7 @@ error_reporting(E_ALL);
 
 <div>
 	<form name="add_new" action="#" method="post">
-		Nome: <input type="text" name="name" /><br>
+		Nomee: <input type="text" name="name" /><br>
 		Telefone: <input type="text" name="telephone" /><br>
 		Rating: <input type="text" name="rating" /><br>
 		Comentario: <input type="text" name="comment" /><br>
