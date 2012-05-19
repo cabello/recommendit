04c228d2affb0fec2167ed5e26d032088ba5d2e6
Thiago Genez <thiagogenez@gmail.com>
2012-05-19 10:46:01 -0300

    util

--- kickstrap/util.php
+++ kickstrap/util.php
@@ -2,10 +2,14 @@
 function phone_mask($mask,$string)
 {
    //$string = str_replace(" ","",$string);
-   for($i=0;$i<strlen($string);$i++)
-   {
+   for($i=0;$i<strlen($string);$i++){
       $mask[strpos($mask,"#")] = $string[$i];
    }
    return $mask;
 }
+
+function remove_non_numeric($string) {
+	return preg_replace('/\D/', '', $string)
+}
+
 ?>
