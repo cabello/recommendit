ad65aec9811616a8d0d004fad74db9f1899091d0
Danilo Cabello <danilo.cabello@gmail.com>
2012-05-19 03:27:41 -0300

    fix old rating sys

--- kickstrap/js/ratingsys.js
+++ kickstrap/js/ratingsys.js
@@ -10,20 +10,17 @@ var rated;
 
 // Rollover for image Stars //
 function rating(num){
-	sMax = 0;	// Isthe maximum number of stars
-	for(n=0; n<num.parentNode.childNodes.length; n++){
-		if(num.parentNode.childNodes[n].nodeName == "A"){
-			sMax++;	
-		}
-	}
-	
+	num = $(this)[0];
+
+	sMax = 5;	// Isthe maximum number of stars
+
 	if(!rated){
 		s = num.id.replace("_", ''); // Get the selected star
 		a = 0;
-		for(i=1; i<=sMax; i++){		
+		for(i=1; i<=sMax; i++){
 			if(i<=s){
 				document.getElementById("_"+i).className = "icon-star";
-				document.getElementById("rateStatus").innerHTML = num.title;	
+				document.getElementById("rateStatus").innerHTML = num.title;
 				holder = a+1;
 				a++;
 			}else{
@@ -35,9 +32,11 @@ function rating(num){
 
 // For when you roll out of the the whole thing //
 function off(me){
+	me = $(this)[0];
+
 	if(!rated){
-		if(!preSet){	
-			for(i=1; i<=sMax; i++){		
+		if(!preSet){
+			for(i=1; i<=sMax; i++){
 				document.getElementById("_"+i).className = "icon-star-empty";
 				document.getElementById("rateStatus").innerHTML = me.parentNode.title;
 			}
@@ -50,6 +49,8 @@ function off(me){
 
 // When you actually rate something //
 function rateIt(me){
+	me = $(this)[0];
+
 	if(!rated){
 		document.getElementById("rateStatus").innerHTML = document.getElementById("ratingSaved").innerHTML + " :: "+me.title;
 		preSet = me;
@@ -61,5 +62,5 @@ function rateIt(me){
 
 // Send the rating information somewhere using Ajax or something like that.
 function sendRate(sel){
-	alert("Your rating was: "+sel.title);
+	$('#rating').val(sel.id.replace('_', ''));
 }
