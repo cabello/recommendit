e8ad9ee0d9a7ebbdc4c1b5576fa8765d1e2ed39b
vinicius flores <fzvinicius@gmail.com>
2012-05-19 12:07:17 -0300

    a

--- new_worker.php
+++ new_worker.php
@@ -11,7 +11,44 @@ if(count($_POST)){
         die(mysql_error());
     }
 }
-
 // TODO: Protect against SQL injection
-
 ?>
+<html xmlns="http://www.w3.org/1999/xhtml"
+xmlns:fb="https://www.facebook.com/2008/fbml">
+<head>
+<title>Share your recommendation!</title>
+</head>
+<body>
+<div id='fb-root'></div>
+<script src='http://connect.facebook.net/en_US/all.js'></script>
+<p><a onclick='postToFeed(); return false;'>Post to Feed</a></p>
+<p id='msg'></p>
+
+<script> 
+FB.init({appId: <? echo $_SESSION["appid"]; ?>, status: true, cookie: true});
+
+function postToFeed() {
+
+	// calling the API ...
+	var obj = {
+method: 'feed',
+	link: 'https://developers.facebook.com/docs/reference/dialogs/',
+	picture: 'http://fbrell.com/f8.jpg',
+	name: 'Facebook RecommendIt',
+	caption: 'RecommendIt!',
+	description: 'Share you your friends.'
+	};
+
+	function callback(response) {
+		document.getElementById('msg').innerHTML = "Post ID: " + response['post_id'];
+	}
+
+	FB.ui(obj, callback);
+}
+
+</script>
+</body>
+</html>
+
+
+
