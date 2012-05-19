0630a89e9df13a85c1486b582cc4300c969bbcac
Thiago Genez <thiagogenez@gmail.com>
2012-05-19 06:18:05 -0300

    add util.php

---
+++ kickstrap/util.php
@@ -0,0 +1,11 @@
+<?php
+function phone_mask($mask,$string)
+{
+   $string = str_replace(" ","",$string);
+   for($i=0;$i<strlen($string);$i++)
+   {
+      $mask[strpos($mask,"#")] = $string[$i];
+   }
+   return $mask;
+}
+?>
