a73b9f0b7283d96fa587a807d29f64f0b0039567
vinicius flores <fzvinicius@gmail.com>
2012-05-18 22:51:55 -0300

    a

--- test/index.php
+++ test/index.php
@@ -25,7 +25,7 @@ if (isset($actual_code)) {
 if (isset($_SESSION["token"])) {
 	$query = "https://graph.facebook.com/me?access_token=".$_SESSION["token"];
 	$answer = file_get_contents($query);	
-	$echo "resposta:[".$answer."]<br>";
+	echo "resposta:[".$answer."]<br>";
 }
 
 
