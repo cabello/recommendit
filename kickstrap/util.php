59f791fd51935a5a772807983804719f80959018
Thiago Genez <thiagogenez@gmail.com>
2012-05-19 06:23:14 -0300

    fix mask

--- kickstrap/util.php
+++ kickstrap/util.php
@@ -1,7 +1,7 @@
 <?php
 function phone_mask($mask,$string)
 {
-   $string = str_replace(" ","",$string);
+   //$string = str_replace(" ","",$string);
    for($i=0;$i<strlen($string);$i++)
    {
       $mask[strpos($mask,"#")] = $string[$i];
