dd53d07c813c14c2fbaf903ee7b415c162335b37
Danilo Cabello <danilo.cabello@gmail.com>
2012-05-19 12:12:58 -0300

    refactor rating system

--- js/ratingsys.js
+++ js/ratingsys.js
@@ -3,65 +3,57 @@ Author: Addam M. Driver
 Date: 10/31/2006
 */
 
-var sMax;	// Isthe maximum number of stars
+var sMax = 5;	// Isthe maximum number of stars
 var preSet; // Is the PreSet value onces a selection has been made
 var rated;
 
 // Rollover for image Stars //
-function rating(num){
-	num = $(this)[0];
+function rating(e){
+	node = $(e.target);
 
-	sMax = 5;	// Isthe maximum number of stars
-
-	s = num.id.replace("_", ''); // Get the selected star
-	for(i=1; i<=sMax; i++){
-		if(i<=s){
-			document.getElementById("_"+i).className = "icon-star";
-			document.getElementById("rateStatus").innerHTML = num.title;
-		}else{
-			document.getElementById("_"+i).className = "icon-star-empty";
+	node.siblings('a').each(function(i, element) {
+		if (element.data('rating') <= node.data('rating')) {
+			element.removeClass('icon-star-empty');
+			element.addClass('icon-star');
+			node.siblings('.rateStatus').html(node.attr('title'));
+		} else {
+			element.removeClass('icon-star');
+			element.addClass('icon-star-empty');
 		}
-	}
+	});
 }
 
 // For when you roll out of the the whole thing //
-function off(me){
-	me = $(this)[0];
+function off(e){
+	node = $(e.target);
 
-	if(!preSet){
-		for(i=1; i<=sMax; i++){
-			document.getElementById("_"+i).className = "icon-star-empty";
-			document.getElementById("rateStatus").innerHTML = me.parentNode.title;
-		}
-	}else{
-		rating(preSet);
-		document.getElementById("rateStatus").innerHTML = document.getElementById("ratingSaved").innerHTML;
-	}
+	node.siblings('a').each(function(i, element) {
+		element.removeClass('icon-star');
+		element.addClass('icon-star-empty');
+		node.siblings('.rateStatus').html('');
+	});
 }
 
 // When you actually rate something //
-function rateIt(me){
-	me = $(this)[0];
+function rateIt(e){
+	node = $(e.target);
 
-	document.getElementById("rateStatus").innerHTML = me.title;
-	preSet = me;
-	rated=1;
-	sendRate(me);
-	rating(me);
+	node.siblings('.rateStatus').html(node.attr('title'));
+	sendRate(node);
+	rating(e);
 }
 
 // Send the rating information somewhere using Ajax or something like that.
-function sendRate(sel){
-	var number = sel.id.replace('_', '');
-	$('#rating').val(number);
+function sendRate(node){
+	node.siblings('[name="rating"]').val(node.data('rating'));
 	rated = number;
 }
 
 function resetRate() {
 	rated=0;
-	$('#rateMe a').removeClass('icon-star');
-	$('#rateMe a').addClass('icon-star-empty');
-	$('#rateMe #rateStatus').html('');
+	$('.rateMe a').removeClass('icon-star');
+	$('.rateMe a').addClass('icon-star-empty');
+	$('.rateMe .rateStatus').html('');
 }
 
 function setRate(number) {
--- workers.php
+++ workers.php
@@ -191,15 +191,15 @@ function stars($rating) {
           <form class="form-inline" id='new-worker-form'>
             <input type="text" class="input-large name" placeholder="Name" name="name" id="new-worker-name" />
             <input type="text" class="input-small" placeholder="Phone" name="phone" id="new-worker-phone" />
-            <input type="hidden" name="rating" id="rating" />
-              <div id="rateMe" title="Rate Me..." class="rating">
-                <a id="_1" title="ehh..." class="icon-star-empty"></a>
-                <a id="_2" title="Not Bad" class="icon-star-empty"></a>
-                <a id="_3" title="Pretty Good" class="icon-star-empty"></a>
-                <a id="_4" title="Out Standing" class="icon-star-empty"></a>
-                <a id="_5" title="Freakin' Awesome!" class="icon-star-empty"></a>
-                <span id="rateStatus"></span>
-                <span id="ratingSaved"></span>
+              <div id="rateMe" title="Rate Me..." class="rating rateMe">
+                <input type="hidden" name="rating" id="rating" />
+                <a title="ehh..." class="icon-star-empty" data-rating="1"></a>
+                <a title="Not Bad" class="icon-star-empty" data-rating="2"></a>
+                <a title="Pretty Good" class="icon-star-empty" data-rating="3"></a>
+                <a title="Out Standing" class="icon-star-empty" data-rating="4"></a>
+                <a title="Freakin' Awesome!" class="icon-star-empty" data-rating="5"></a>
+                <span id="rateStatus" class="rateStatus"></span>
+                <span id="ratingSaved" class="rateSaved"></span>
               </div>
 
             <input type="hidden" name="id_service" id="new-worker-id_service" value="1" />
@@ -220,15 +220,15 @@ function stars($rating) {
           <form class="new-comment-form">
             <p class="rating">
               <span id="old_worker_message"></span>
-              <input type="hidden" name="rating" id="rating2" />
-              <div id="rateMe2" title="Rate Me..." class="rating">
-                <a id="_1" title="ehh..." class="icon-star-empty"></a>
-                <a id="_2" title="Not Bad" class="icon-star-empty"></a>
-                <a id="_3" title="Pretty Good" class="icon-star-empty"></a>
-                <a id="_4" title="Out Standing" class="icon-star-empty"></a>
-                <a id="_5" title="Freakin' Awesome!" class="icon-star-empty"></a>
-                <span id="rateStatus2"></span>
-                <span id="ratingSaved2"></span>
+              <div id="rateMe2" title="Rate Me..." class="rating rateMe">
+                <input type="hidden" name="rating" id="rating2" />
+                <a title="ehh..." class="icon-star-empty" data-rating="1"></a>
+                <a title="Not Bad" class="icon-star-empty" data-rating="2"></a>
+                <a title="Pretty Good" class="icon-star-empty" data-rating="3"></a>
+                <a title="Out Standing" class="icon-star-empty" data-rating="4"></a>
+                <a title="Freakin' Awesome!" class="icon-star-empty" data-rating="5"></a>
+                <span id="rateStatus2" class="rateStatus"></span>
+                <span id="ratingSaved2" class="rateSaved"></span>
               </div>
             </p>
             <textarea class="input-xlarge comment" placeholder="Comment" rows="3" name="comment" id="new-comment-comment"></textarea>
