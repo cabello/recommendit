f301a8abd765f694f6e1f20727aeba69ca1e0e6c
vinicius flores <fzvinicius@gmail.com>
2012-05-18 21:28:11 -0300

    a

--- test/login.php
+++ test/login.php
@@ -9,7 +9,7 @@ xmlns:fb="https://www.facebook.com/2008/fbml">
 <?
 	$app_id = "362571837137094";
 	$index_url = "http://fb-hacktoon.herokuapp.com/";
-	$permittion_names = "read_friendlists";
+	$permittion_names = "read_friendlists,read_insights";
 	$last_param = "_unique_are_you";
 ?>
 window.fbAsyncInit = function() {
