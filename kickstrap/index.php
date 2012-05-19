869a73423a3a38c49ac96249373de9a9a89372d1
vinicius flores <fzvinicius@gmail.com>
2012-05-19 07:54:36 -0300

    dfsa

--- kickstrap/index.php
+++ kickstrap/index.php
@@ -48,10 +48,10 @@
             <div class="span2">&nbsp;</div>
             <div class="span8">
               <div class="hero-unit">
-                <h1><i class="icon-list"></i> hacktoon</h1>
-                <h2>De amigo pra amigo.</h2>
+                <h1><i class="icon-list"></i> Fb'em'all</h1>
+                <h2>|Friend -> friend.</h2>
 
-                <p style="text-align: center; padding: 25px;"><a class="btn btn-primary btn-large" id="roll" style="display:none;" href="workers.php">Lets roll &raquo;</a></p>
+                <p style="text-align: center; padding: 25px;"><a class="btn btn-primary btn-large" id="roll" style="display:none;" href="workers.php">Lets roll! &raquo;</a></p>
 <div id="fb-root"></div>
 <script>
 <?php
